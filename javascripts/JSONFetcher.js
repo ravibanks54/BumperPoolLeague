aa64b298fc0a0117c2dd1f9eeec299f0aaa1416f
Ravi Bhankharia <ravibanks54@gmail.com>
2016-06-29 19:04:46 -0400

    Cleaned up template, removed elo from JSONFetcher

--- javascripts/JSONFetcher.js
+++ javascripts/JSONFetcher.js
@@ -1,17 +1,3 @@
-function getRatingDelta(myRating, opponentRating, myGameResult) {
-    if ([0, 0.5, 1].indexOf(myGameResult) === -1) {
-      return null;
-    }
-    
-    var myChanceToWin = 1 / ( 1 + Math.pow(10, (opponentRating - myRating) / 400));
-
-    return Math.round(32 * (myGameResult - myChanceToWin));
-  }
-
-  function getNewRating(myRating, opponentRating, myGameResult) {
-    return myRating + getRatingDelta(myRating, opponentRating, myGameResult);
-  }
-
 $.getJSON('https://spreadsheets.google.com/feeds/list/1ZC7Zb1yLamdp0E0Xol-gGO0YPvyJrVdK6eSikIYGRzs/1/public/full?alt=json', function(data) {
 	var playerNames = ["Ravi", "Anish", "Blake", "Tyler", "Matt", "Ryan"];
 	var dictElo = {};
@@ -26,7 +12,7 @@ $.getJSON('https://spreadsheets.google.com/feeds/list/1ZC7Zb1yLamdp0E0Xol-gGO0YP
     var firstBallIn =  entry.gsx$stballin.$t;
     var ballsSunkWinner = 5;
     var ballsSunkLoser = parseInt( entry.gsx$ballssunk-loser.$t);
-    var delta = getRatingDelta(dictElo[winner], dictElo[loser], 1);
+    var delta = Elo.getRatingDelta(dictElo[winner], dictElo[loser], 1);
     dictElo[winner] += delta;
     dictElo[loser] -= delta;
     
