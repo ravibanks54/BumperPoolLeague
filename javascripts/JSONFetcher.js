b4f110ad745ee7249d8851cfa59d2c784d8279c9
Ravi Bhankharia <ravibanks54@gmail.com>
2016-06-29 00:16:44 -0400

    Put Elo code with JSONFetcher

--- javascripts/JSONFetcher.js
+++ javascripts/JSONFetcher.js
@@ -1,3 +1,17 @@
+function getRatingDelta(myRating, opponentRating, myGameResult) {
+    if ([0, 0.5, 1].indexOf(myGameResult) === -1) {
+      return null;
+    }
+    
+    var myChanceToWin = 1 / ( 1 + Math.pow(10, (opponentRating - myRating) / 400));
+
+    return Math.round(32 * (myGameResult - myChanceToWin));
+  }
+
+  function getNewRating(myRating, opponentRating, myGameResult) {
+    return myRating + getRatingDelta(myRating, opponentRating, myGameResult);
+  }
+
 $.getJSON('https://spreadsheets.google.com/feeds/list/1ZC7Zb1yLamdp0E0Xol-gGO0YPvyJrVdK6eSikIYGRzs/1/public/full?alt=json', function(data) {
 	var playerNames = ["Ravi", "Anish", "Blake", "Tyler", "Matt"];
 	var dictElo = {};
