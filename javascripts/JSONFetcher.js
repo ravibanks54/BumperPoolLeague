3d249d299c617a12c187e3f602200f7e555c1cf4
Ravi Bhankharia <ravibanks54@users.noreply.github.com>
2016-06-30 14:28:57 -0400

    Add Gautam

--- javascripts/JSONFetcher.js
+++ javascripts/JSONFetcher.js
@@ -1,5 +1,5 @@
 $.getJSON('https://spreadsheets.google.com/feeds/list/1ZC7Zb1yLamdp0E0Xol-gGO0YPvyJrVdK6eSikIYGRzs/1/public/full?alt=json', function(data) {
-	var playerNames = ["Ravi", "Anish", "Blake", "Tyler", "Matt", "Ryan"];
+	var playerNames = ["Ravi", "Anish", "Blake", "Tyler", "Matt", "Ryan", "Gautam"];
 	var dictElo = {};
 	for(var i = 0; i < playerNames.length; i++){
 		dictElo[playerNames[i]] = 1200;
